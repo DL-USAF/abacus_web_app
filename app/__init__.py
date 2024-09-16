5d47875fb8fe418cac07435543d59cb6b3235af3
James Rider <james.rider.3@us.af.mil>
2024-09-16 13:02:05 -0600

    I think query is done. Results TBD

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,5 @@
 import logging
+from importlib.util import find_spec
 
 from flask import Flask
 
@@ -22,7 +23,9 @@ oidc = auth_service.init_oidc(app)
 
 @app.context_processor
 def inject_oidc():
-    return {'oidc':oidc}
+    mock_datawave = find_spec('datawave_cli') is None
+    
+    return {'oidc':oidc, 'mocking_datawave_cli':mock_datawave}
 
 def create_app():
     from app.routes import main
--- app/auth_services/AuthServiceBase.py
+++ app/auth_services/AuthServiceBase.py
@@ -30,8 +30,7 @@ class CustomOpenIDConnect(OpenIDConnect):
 
 
 def load_auth_service():
-    # auth_service = os.getenv('AUTH_SERVICE', 'MockAuthService')
-    auth_service = 'DexAuthService'
+    auth_service = os.getenv('AUTH_SERVICE', 'MockAuthService')
 
     try:
         module = importlib.import_module(f'app.auth_services.{auth_service}')
